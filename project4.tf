c4e658f23d31002a420478f13d001fa877f9902a
umeetakaur <74040871+umeetakaur@users.noreply.github.com>
2022-04-01 15:40:48 +0800

    Update project4.tf

--- project4.tf
+++ project4.tf
@@ -6,3 +6,7 @@ resource "azurerm_resource_group" "example" {
   name     = "rg_strg"
   location = "eastus"
 }
+resource "azurerm_resource_group" "example1" {
+  name     = "rg_lab4"
+  location = "eastus"
+}
